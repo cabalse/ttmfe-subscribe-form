124fd02ca7a76b1dcc49dfee5dbd95b1d44ea8f4
Jorgen Bengtsson <jorgen@pm-production.nu>
2025-04-19 17:20:45 +0200

    Add Tailwind and build form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
 
-import "./App.css";
-import { TTMFEButton } from "@cabalse/ttmfe-component-lib";
+import { TTMFEButton, TTMFETextInput } from "@cabalse/ttmfe-component-lib";
+import "@cabalse/ttmfe-component-lib/styles.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [email, setMail] = useState("");
+
+  const handleEMailChange = (value) => {
+    setMail(value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Submit Email", email);
+  };
 
   return (
-    <>
-      <TTMFEButton
-        label={`Click Me ${count}`}
-        onClick={() => setCount((prev) => prev + 1)}
-      />
-    </>
+    <div className="bg-slate-200 rounded-lg p-4">
+      <div className="text-black font-bold text-2xl mb-4">Subscribe</div>
+      <div className="flex flex-row">
+        <div className="">
+          <TTMFETextInput
+            placeholder="Enter your email"
+            onChange={handleEMailChange}
+          />
+        </div>
+        <div className="ml-2">
+          <TTMFEButton label={`Submit Email`} onClick={handleSubmit} />
+        </div>
+      </div>
+    </div>
   );
 }
 
